5fbded8ea1b1c0c39919d620d6a7c196ad1ea0a9
Mahsa Dehghani <mahsa.dehghani.s@gmail.com>
2023-10-12 09:52:50 -0400

    Initial Commit

---
+++ HarmonyFest/HarmonyFestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonyFestApp.swift
+//  HarmonyFest
+//
+//  Created by Mahsa Sanij on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HarmonyFestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
